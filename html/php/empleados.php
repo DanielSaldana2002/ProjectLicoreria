dae142cb4e68844610bd3f7a8ab3a5c92e89e365
moisesberumen <83853099+moisesberumen@users.noreply.github.com>
2023-09-29 01:04:35 -0600

    interfas de empleado

---
+++ html/php/empleados.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Empleados</title>
+        <link rel="stylesheet" href="/css/index.css">
+    </head>
+    
+    
+    <body>
+    <form>
+            
+    </form>
+    <div id="img2">
+        <h1 id="titulo">¡Vinos y licores Lopez!</h1>
+        <p></p>
+    </div>
+    <div id="nombre">
+        <ul id="lis">
+            <input id="imp" type="text" placeholder="Nombre"requiored >
+            <input id="imp" type="text" placeholder="Apellido paterno"requiored>
+            <input id="imp" type="text" placeholder="Apellido Materno"requiored>
+            <input id="imp" type="text" placeholder="Correo Electronico"requiored>
+            <input id="imp" type="text" placeholder="Contraseña"requiored>
+        </ul>
+    </div> 
+    <div>   
+        <ul id="bot">
+            <button class="boton">Agregar</button></li>
+            <button class="boton">Borrar</button></li>
+            <button class="boton">Modificar</button></li>
+        </ul>
+    </div>    
+    
+        
+    </body>
+    </html> 
